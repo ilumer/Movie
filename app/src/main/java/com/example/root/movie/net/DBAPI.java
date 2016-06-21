98a7bde90a722595a03d6d5d54313886bd3fcbab
ilumer <18888106831qxq@gmail.com>
2016-06-21 21:19:41 +0800

    fix page problem

--- app/src/main/java/com/example/root/movie/net/DBAPI.java
+++ app/src/main/java/com/example/root/movie/net/DBAPI.java
@@ -1,7 +1,7 @@
 package com.example.root.movie.net;
 
 public class DBAPI {
-    public static final String API_KEY = "de9135e2a9b104a1812bda7ea4fa18a1";
+    public static final String API_KEY = "";
     public static final String BASEIMAGE_URI = "http://image.tmdb.org/t/p/w185";
     public static final String BASEPOPLULAR_URI = "http://api.themoviedb.org/3/movie/popular";
     public static final String BASEMOVIEINFO_URI = "https://api.themoviedb.org/3/movie/";
